f76821a4a05a37aef688415af4effda2d5b8afc9
AK <achikat@gmail.com>
2025-04-17 04:21:38 +0530

    fix

--- src/components/AuthNavigation.tsx
+++ src/components/AuthNavigation.tsx
@@ -31,9 +31,9 @@ export default function AuthNavigation() {
       </Link>
       
       {/* Bottom Navigation */}
-      <nav className="flex-none bg-gray-900 border-t border-gray-700 h-16">
-        <div className="max-w-md mx-auto px-4">
-          <div className="flex justify-between items-center h-16">
+      <nav className="fixed bottom-0 left-0 right-0 bg-gray-900 border-t border-gray-700 h-16 z-50">
+        <div className="max-w-md mx-auto px-4 h-full">
+          <div className="flex justify-between items-center h-full">
             <Link href="/groups" className="flex flex-col items-center text-gray-400 hover:text-white">
               <svg className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
